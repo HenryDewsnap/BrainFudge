5d0b77047008c8fc9d009fb4862bfbe016eff975
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2021-11-17 21:38:04 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <map>
 
 class interpreter {
-    //Brain Fuck Functions.
+    //Brain fuDGE Functions.
     private:
         void imp() { memory_pointer += 1; if (memory_pointer >= 256) { memory_pointer = 0; } }; //Incriment Mem Pointer
         void dmp() { memory_pointer -= 1; if (memory_pointer < 0) { memory_pointer =  255; } }; //Decrement Mem Pointer
@@ -12,10 +12,24 @@ class interpreter {
         void ivs() { memory.at(memory_pointer) += 1; }; //Increase Value Stored
         void dvs() { memory.at(memory_pointer) -= 1; }; //Decrease Value Stored
 
-        void slp() { loopLayer+=1; loopStarts.push_back(instruction_pointer); }; //Start A Loop (Executes until the mem pointers addressed value == 0)
-        void elp() { if (memory.at(memory_pointer) == 0) { loopLayer -= 1; loopStarts.pop_back(); } else { instruction_pointer = loopStarts.back(); } }; //End A Loop / return backto start.
+        void slp() { 
+            loopLayer+=1; 
+            loopStarts.push_back(instruction_pointer); 
+            conditionAddr.push_back(memory_pointer);
+            }; //Start A Loop (Executes until the mem pointers addressed value == 0)
 
-        void gtc() { std::string inpStr; std::vector<char> inpStrVec; std::cin >> inpStr; for (char c: inpStr) { inpStrVec.push_back(c); } memory.at( memory_pointer ) = (int)inpStrVec.at(0); }; //Get character (reads one character from the console/cli)
+        void elp() { 
+            if (memory.at(conditionAddr.at(loopLayer-1)) == 0) { 
+                loopLayer -= 1; 
+                loopStarts.pop_back(); 
+                conditionAddr.pop_back(); 
+            }
+            else { 
+                instruction_pointer = loopStarts.back(); 
+                }
+            }; //End A Loop / return backto start.
+
+        void gtc() { std::cin >> memory.at(memory_pointer); }; //Get character (reads one character from the console/cli)
         void ptc() { std::cout << (char)memory.at(memory_pointer) << std::endl; }; //Put character (prints one character to the console/cli)
 
     //Interpreter Variables.
@@ -26,6 +40,7 @@ class interpreter {
         std::string program_data;
 
         std::vector<int> loopStarts; //This takes the loop depth and returns their initial functions address allowing for nested recursion.
+        std::vector<int> conditionAddr;
         int loopLayer = 0;
 
     public:
@@ -40,40 +55,26 @@ class interpreter {
 
 void interpreter::interpret() {
     while (instruction_pointer <= program_data.size()) {
-        if (logging) {
-            std::cout << "[BrainFuck]: Instruction Pointer: " << instruction_pointer << std::endl;
-            std::cout << "[BrainFuck]: Memory Pointer: " << memory_pointer << std::endl;
-            std::cout << "[BrainFuck]: Current Address Value: " << memory.at(memory_pointer) << std::endl;
-        }
-
         std::string currentInst(1, program_data[instruction_pointer]);
-        
+
+        if (logging == true) {
+            std::cout << "" << std::endl;
+            std::cout << "[BrainFuDGE]: Instruction Pointer: " << instruction_pointer << std::endl;
+            std::cout << "[BrainFuDGE]: Memory Pointer: " << memory_pointer << std::endl;
+            std::cout << "[BrainFuDGE]: Current Address Value: " << memory.at(memory_pointer) << std::endl;
+            std::cout << "[BrainFuDGE]: Current Instruction: " << currentInst <<  std::endl;
+            std::cout << "" << std::endl;
+        }
 
         //Im aware i should use a switch/case statement but they dont appear to like me very much...
-        if (currentInst == ">") {
-            imp();
-        }
-        if (currentInst == "<") {
-            dmp();
-        }
-        if (currentInst == "+") {
-            ivs();
-        }
-        if (currentInst == "-") {
-            dvs();
-        }
-        if (currentInst == "[") {
-            slp();
-        }
-        if (currentInst == "]") {
-            elp();
-        }
-        if (currentInst == ",") {
-            gtc();
-        }
-        if (currentInst == ".") {
-            ptc();
-        }
+        if (currentInst == ">") { imp(); }
+        if (currentInst == "<") { dmp(); } 
+        if (currentInst == "+") { ivs(); } 
+        if (currentInst == "-") { dvs(); } 
+        if (currentInst == "[") { slp(); } 
+        if (currentInst == "]") { elp(); } 
+        if (currentInst == ",") { gtc(); } 
+        if (currentInst == ".") { ptc(); }
 
         instruction_pointer += 1;
     }
@@ -81,16 +82,32 @@ void interpreter::interpret() {
 
 void interpreter::loadProgram(std::string program) {
     program_data = program;
-    std::cout << "[BrainFuck]: Program Loaded" << std::endl;
+    std::cout << "[BrainFuDGE]: Program Loaded" << std::endl;
 }
+
+
 
 int main() {
-    interpreter brainFuckTest;
-    brainFuckTest.logging = true;
-    
-    brainFuckTest.loadProgram(
-        ""
-        );
+    interpreter brainFuDGETest;
+    brainFuDGETest.logging = false;
 
-    brainFuckTest.interpret();
+    brainFuDGETest.loadProgram("+++++[>+++++[.-]<-]");
+
+    brainFuDGETest.interpret();
 }
+/* Example Program
+"+++++[>+++++[.-]<-]"
+
+
+mem[0] = 5;
+while mem[0] != 0{
+    mem[1] = 5;
+    while mem[1] != 0{
+        out(mem[1])
+        mem[1] -= 1;
+    }
+    mem[0] -= 1;
+}
+
+This program has no purpose, it is just here for demonstrational purposes and to demonstrate the interpreteres ability to nest loops.
+*/
